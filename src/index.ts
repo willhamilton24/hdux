25ca7ae292955568441f39b3a08c073b7d92f098
Will Hamilton <ishi121@icloud.com>
2026-03-13 15:58:38 -0400

    Initial HDUX project setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// Hamilton Dynamic User Experience (HDUX)
+// React Component Framework
+
+// Components
+// export { Button } from "./components/Button";
+
+// Hooks
+// export { useTheme } from "./hooks/useTheme";
+
+// Utils
+// export { cx } from "./utils/cx";
